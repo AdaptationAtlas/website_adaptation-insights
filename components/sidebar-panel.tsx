65ca5aa7316ed2d09c3c2c524e1c5808141dce6a
Nick Harbaugh <nick@sevenmilemedia.com>
2023-11-07 20:09:45 -0600

    Additional test content and conditional display logic

--- components/sidebar-panel.tsx
+++ components/sidebar-panel.tsx
@@ -1,54 +1,16 @@
 'use client'
 
-import React, { ReactNode, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 import { formatNumberCommas } from '@/lib/utils'
 import Switch from '@/components/ui/switch'
-import { useRouter, usePathname } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation'
 import Router from "next/router";
+import { testProjects, testActors } from '@/lib/test-data'
 
 type Props = {
   page: string;
   slug: string;
 };
-
-const testData = [
-  {
-    'id': 0,
-    'title': 'Farmers’ Organizations for Africa, Caribbean and Pacific',
-    'beneficiaries': 52000000,
-    'budget': 5820000
-  },
-  {
-    'id': 1,
-    'title': 'Forest and Farm Facility Phase 1&2',
-    'beneficiaries': 40000000,
-    'budget': 96500000
-  },
-  {
-    'id': 2,
-    'title': 'Sustainable management of dryland landscapes in Burkina Faso',
-    'beneficiaries': 300000,
-    'budget': 6430000
-  },
-  {
-    'id': 3,
-    'title': 'Prepared for the impact of climate change',
-    'beneficiaries': 6300,
-    'budget': 27000
-  },
-  {
-    'id': 4,
-    'title': 'L’Observatoire de l’Agroecologie au Maroc',
-    'beneficiaries': null,
-    'budget': 6400
-  },
-  {
-    'id': 5,
-    'title': 'PA categories V and VI as landscape mechanisms for enhancing biodiversity in agricultural land, ecological connectivity and REDD+ implementation',
-    'beneficiaries': 38000,
-    'budget': 42700000
-  }
-]
 
 const SidebarPanel = ({ page, slug }: Props) => {
   const router = useRouter()
@@ -59,6 +21,7 @@ const SidebarPanel = ({ page, slug }: Props) => {
   // Reference: How to make Radix UI Tabs URL based in NextJS
   // https://dev.to/yinks/how-to-make-radix-ui-tabs-url-based-in-nextjs-2nfn
   // TODO - consider using query parameters here to test for performance
+  // TODO - try using next/router for better performance (what's the difference?)
 
   const handleSwitchToggle = (checked: boolean) => {
     const view = checked ? '/projects' : '/partners';
@@ -67,7 +30,7 @@ const SidebarPanel = ({ page, slug }: Props) => {
   }
 
   // if the query parameter changes, update the state
-  React.useEffect(() => {
+  useEffect(() => {
     const view = (pathname == '/projects') ? true : false;
     setViewProjects(view)
   }, [pathname])
@@ -91,20 +54,73 @@ const SidebarPanel = ({ page, slug }: Props) => {
           options={['Beneficiaries', 'Budget']}
           isLarge={false}
         />
-        <span className='uppercase text-sm'>During</span>
+        {viewProjects &&
+          <div className='flex justify-between px-5 py-5'>
+            <span className='uppercase text-sm'>During</span>
+          </div>
+        }
+        {viewProjects && viewByBudget &&
+          <div className='flex justify-between px-5 py-5'>
+            <span className='uppercase text-sm'>Currency</span>
+          </div>
+        }
+        {!viewProjects && viewByBudget &&
+          <div className='flex justify-between px-5 py-5'>
+            <span className='uppercase text-sm'>Currency</span>
+          </div>
+        }
       </header>
       <div>
-        <div className='flex justify-between px-5 py-5'>
-          <p className='uppercase'>All countries</p>
-          <p className='uppercase'>200 of 200 projects</p>
-        </div>
+        {viewProjects &&
+          <div className='flex justify-between px-5 py-5'>
+            <p className='uppercase'>All countries</p>
+            <p className='uppercase'>200 of 200 projects</p>
+          </div>
+        }
+        {!viewProjects &&
+          <div className='flex justify-between px-5 py-5'>
+            <p className='uppercase'>All partners</p>
+            <p className='uppercase'>200 of 200 partners</p>
+          </div>
+        }
         <div className='flex flex-col overflow-x-auto'>
-          {testData.map(project => {
+          {viewProjects && testProjects.map(project => {
             return (
-              <div key={project.id} className='px-5 my-5'>
-                <h3 className='uppercase text-xs'>Project budget</h3>
-                <p className='text-5xl font-bold'>${formatNumberCommas(project.budget)}</p>
+              <div key={project.projectCode} className='px-5 my-5'>
+                {viewByBudget &&
+                  <div>
+                    <h3 className='uppercase text-xs'>Project budget</h3>
+                    <p className='text-5xl font-bold'>${formatNumberCommas(project.budget)}</p>
+                  </div>
+                }
+                {!viewByBudget &&
+                  <div>
+                    <h3 className='uppercase text-xs'>Project beneficiaries</h3>
+                    {/* TODO - account for unspecified beneficiaries */}
+                    <p className='text-5xl font-bold'>{formatNumberCommas(project.beneficiaries)}</p>
+                  </div>
+                }
                 <p className='text-sm'>{project.title}</p>
+              </div>
+            )
+          })}
+          {!viewProjects && testActors.map(actor => {
+            return (
+              <div key={actor.actorCode} className='px-5 my-5'>
+                {viewByBudget &&
+                  <div>
+                    <h3 className='uppercase text-xs'>Total budget</h3>
+                    <p className='text-5xl font-bold'>${formatNumberCommas(actor.totalBudget)}</p>
+                  </div>
+                }
+                {!viewByBudget &&
+                  <div>
+                    <h3 className='uppercase text-xs'>Total beneficiaries</h3>
+                    {/* TODO - account for unspecified beneficiaries */}
+                    <p className='text-5xl font-bold'>{formatNumberCommas(actor.totalBeneficiaries)}</p>
+                  </div>
+                }
+                <p className='text-sm'>{actor.title}</p>
               </div>
             )
           })}
