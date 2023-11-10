1c31ef5988f0bc1f426aca3ba4761f96b4fc51f2
Nick Harbaugh <nick@sevenmilemedia.com>
2023-11-10 16:36:37 -0600

    Update bitbucket pipelines

--- next.config.js
+++ next.config.js
@@ -1,14 +1,26 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'cdn.sanity.io', // accept images from sanity.io servers
-        port: '',
-      }
-    ]
-  }
-}
+        hostname: 'cdn.sanity.io',
+      },
+    ],
+  },
+  // Add this if you need to customize the static export behavior
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      // Add more paths here if you have dynamic routes
+    };
+  },
+  // If you're using rewrites, headers, or redirects, you might need to adjust them here
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
