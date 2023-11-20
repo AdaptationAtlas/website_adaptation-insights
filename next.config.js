451b99212a688b777d5cca9593e8c7290555f3b8
Nick Harbaugh <nick@sevenmilemedia.com>
2023-11-20 11:09:56 -0600

    Revert next config for SSR

--- next.config.js
+++ next.config.js
@@ -1,35 +1,14 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-  output: 'export',
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  images: {
-    unoptimized: true,
-  },
-  trailingSlash: true,
-  distDir: 'dist',
-  experimental: {
-    serverComponentsExternalPackages: ['@resvg/resvg-js'],
-  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'cdn.sanity.io',
-      },
-    ],
-  },
-  // Add this if you need to customize the static export behavior
-  // exportPathMap: async function (
-  //   defaultPathMap,
-  //   { dev, dir, outDir, distDir, buildId }
-  // ) {
-  //   return {
-  //     '/': { page: '/' },
-  //     // Add more paths here if you have dynamic routes
-  //   };
-  // },
-};
+        hostname: 'cdn.sanity.io', // accept images from sanity.io servers
+        port: '',
+      }
+    ]
+  }
+}
 
-module.exports = nextConfig;
-
+module.exports = nextConfig
