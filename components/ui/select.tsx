960a28b863ae1cf3519ff3b900f6a64048251dc4
Nick Harbaugh <nick@periscopic.com>
2023-12-11 21:03:05 -0600

    Update dropdown select styles

--- components/ui/select.tsx
+++ components/ui/select.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 import * as SelectPrimitive from "@radix-ui/react-select"
-import { BiChevronDown, BiChevronUp, BiCheck } from "react-icons/bi";
+import { BiChevronDown, BiCheck } from "react-icons/bi";
 
 import classNames from 'classnames'
 
@@ -40,7 +40,9 @@ const SelectContent = React.forwardRef<
     <SelectPrimitive.Content
       ref={ref}
       className={classNames(
-        "relative z-50 min-w-[8rem] bg-white overflow-scroll max-h-[300px] bg-popover text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+        "relative z-50 min-w-[8rem] bg-white overflow-scroll max-h-[300px] bg-popover text-popover-foreground shadow-md",
+        "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+        "-ml-[23px]", // Align content text with trigger text
         position === "popper" &&
           "data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
         className
@@ -68,7 +70,7 @@ const SelectLabel = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SelectPrimitive.Label
     ref={ref}
-    className={classNames("py-1.5 pl-8 pr-2 text-sm font-semibold", className)}
+    className={classNames("py-2 pl-5 pr-2 text-sm font-semibold", className)}
     {...props}
   />
 ))
@@ -81,14 +83,14 @@ const SelectItem = React.forwardRef<
   <SelectPrimitive.Item
     ref={ref}
     className={classNames(
-      "relative flex w-full cursor-pointer select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none text-grey-500 hover:text-grey-700 focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+      "relative flex w-full cursor-pointer select-none items-center rounded-sm py-2 pl-5 pr-2 text-sm outline-none text-grey-500 hover:text-grey-700 focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
       className
     )}
     {...props}
   >
-    <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
+    <span className="absolute left-1 flex h-3 w-3 items-center justify-center">
       <SelectPrimitive.ItemIndicator>
-        <BiCheck className="h-4 w-4" />
+        <BiCheck className="h-3.5 w-3.5" />
       </SelectPrimitive.ItemIndicator>
     </span>
 
